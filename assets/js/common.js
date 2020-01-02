a8f91b4b550961bbeff5e2cf17b272ea12314af6
haoxing49 <1317876297@qq.com>
2020-01-02 20:05:22 +0800

    优化代码高亮

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,3 +4,8 @@ layui.use('util', function() {
         css: { right: 50, bottom: 100 }
     });
 });
+layui.use('code', function() {
+
+    layui.code();
+    //实际使用时， 执行该方法即可。 而此处注释是因为修饰器在别的js中已经执行过了
+});
