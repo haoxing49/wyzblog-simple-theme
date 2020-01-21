1b8f80bb2b8af1657008586369ba9a72f14724e2
haoxing49 <1317876297@qq.com>
2020-01-21 15:02:47 +0800

    首页调整

--- index.php
+++ index.php
@@ -2,63 +2,38 @@
 
 <!-- Column 1 /Content -->
 
-<div class="layui-col-md9 layui-col-lg9">
+<div class="layui-col-md9 layui-col-lg9" id='ajaxpost'>
 	<!-- Blog Post -->
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<div class="list-post list-card layui-col-xs12">
-				<div class="metacat">
-					<?php
-					$category = get_the_category();
-					if ($category[0]) {
-						echo '<a href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->cat_name . '</a>';
-					}
-					?>
-				</div>
-				<div class="list-post-thumb">
-					<a href="">
-						<?php if (is_has_image()) { ?>
-							<img src="<?php echo catch_that_image(); ?>" alt="" class="img-full">
-						<?php } else { ?>
-							<img src="<?php bloginfo('template_url'); ?>/images/thumb/img<?php echo rand(1, 5) ?>.jpg" alt="" class="img-full">
-						<?php } ?>
-					</a>
-				</div>
-
-				<!-- Post Title -->
-				<h2 class="list-post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-				<!-- Post Data -->
-				<div class='list-post-content'>
-					<!-- Post Content -->
-					<div class="post_content">
-						<a href="<?php the_permalink(); ?>" rel="bookmark">
-							<?php the_excerpt(); ?>
-						</a>
-					</div>
-					<p class="post_data">
-						<span class='post-tags'><i class='layui-icon layui-icon-note'></i>
-							<?php the_tags(''); ?>
-						</span>
-
-						<span class='post-date'>
-							<i class='layui-icon layui-icon-date'></i> <?php the_time('Y年n月j日') ?>
-						</span>
-						<span class='post-comment-count'>
-							<i class='layui-icon layui-icon-dialogue'></i> <?php comments_popup_link('0', '1', '%', '', '评论已关闭'); ?>
-						</span>
-						<span class='post-edit-link'>
-							<?php edit_post_link('编辑', ' <i class="layui-icon layui-icon-edit"></i> ', ''); ?>
-						</span>
-					</p>
-				</div>
-			</div>
-		<?php endwhile; ?>
-		<div class="layui-col-xs12 posts-nav">
-			<?php get_page_link_wyzblog(); ?>
-		</div>
-	<?php else : ?>
-		<h2 class="list-post-title"><a href="#" rel="bookmark">未找到</a></h2>
-		<p>没有找到任何文章！</p>
-	<?php endif; ?>
 </div>
+
+<script>
+	<?php global $wp_query; ?>
+	layui.use('flow', function() {
+		var $ = layui.jquery; //不用额外加载jQuery，flow模块本身是有依赖jQuery的，直接用即可。
+		var flow = layui.flow;
+		flow.load({
+			elem: '#ajaxpost', //指定列表容器
+			isAuto: 'false',
+			end:'<span>—— 这是我的底线 ——</span>',
+			isLazyimg:'true',
+			done: function(page, next) { //到达临界点（默认滚动触发），触发下一页 
+				//console.log(page);
+				$.ajax({
+					type: 'POST',
+					url: '<?php echo admin_url('admin-ajax.php'); ?>',
+					dataType: "html", // add data type
+					data: {
+						action: 'get_ajax_posts',
+						page_now: page//当前页
+					},
+					success: function(strOut) {
+						//console.log(strOut);	
+						next(strOut, page < <?php echo $wp_query->max_num_pages; ?>);
+					}
+				});
+			}
+		});
+	});
+</script>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
